7152790d2b92d03acc9ac22611487d33c27e95dc
chuck francis <chuck_francis10@yahoo.com>
2020-08-13 15:19:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DifferentKettleOfFish08132020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fish = 1;
+
+            while (fish < 11)
+            {
+                if (fish == 3)
+                {
+                    Console.WriteLine("RED fish!");
+                }
+                else if (fish == 4)
+                {
+                    Console.WriteLine("Blue fish!");
+                }
+                else
+                {
+                    Console.WriteLine(fish + " fish!");
+                }
+
+                fish++;
+            }
+        }
+    }
+}
